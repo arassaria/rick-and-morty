99587b7bbaa36e8d0a5963039ec8953d7db1f876
domko95 <dominique.kolbe1995@googlemail.com>
2020-10-21 15:04:47 +0200

    Add first 20 Characters on load

--- src/App.js
+++ src/App.js
@@ -1,29 +1,36 @@
 import "./app.css";
 import Character from "./components/Character";
+import Characters from "./components/Characters";
 import Header from "./components/Header";
-import { getCharacterById } from "./utils/api";
+import { getCharacters } from "./utils/api";
 import { createElement } from "./utils/elements";
 
 function App() {
   const header = Header();
 
+  const charactersContainer = Characters();
   const main = createElement("main", {
     className: "main",
+    children: [charactersContainer],
   });
 
-  async function getCharacters(id) {
-    const character = await getCharacterById(id);
-    main.append(
+  async function loadCharacters() {
+    const characters = await getCharacters();
+    const characterElements = characters.map((character) =>
       Character({
         name: character.name,
         imgSrc: character.image,
       })
     );
+    // const character = await getCharacterById(id);
+    charactersContainer.append(...characterElements);
   }
 
-  for (let i = 1; i <= 10; i++) {
-    getCharacters(i);
-  }
+  // for (let i = 1; i <= 10; i++) {
+  //   loadCharacters(i);
+  // }
+
+  loadCharacters();
 
   const container = createElement("div", { children: [header, main] });
   return container;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,10 +1,13 @@
 import { createElement } from "../utils/elements";
+import "./character.css";
 
 function Character({ name, imgSrc }) {
-  const title = createElement("div", {
+  const title = createElement("p", {
+    className: "character__title",
     innerText: name,
   });
   const avatar = createElement("img", {
+    className: "character__img",
     src: imgSrc,
     alt: name,
   });
